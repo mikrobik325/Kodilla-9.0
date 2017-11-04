b24fc4cb83cfad485eb8101d6074ba04ae20e524
Robert Kuczyński <robertkuczynski@gmail.com>
2017-11-04 18:38:23 +0100

    skrypt poprawiony: zwraca prawidłowe dane

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,14 @@
-var triangle1Area = getTriangleArea (10, 15);
-var triangle2Area = getTriangleArea (4, 5);
-var triangle3Area = getTriangleArea (-1, 12);
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(4, 5);
+var triangle3Area = getTriangleArea(-1, 12);
 
 function getTriangleArea(a, h) {
-  if ((a > 0) && (h > 0)) {
-  } else {  
-  console.log("Nieprawidłowe dane");
+  if (a > 0 && h > 0) {
+    return a * h / 2;
+  } else {
+    //console.log("Nieprawidłowe dane");
+    return "Nieprawidłowe dane";
   }
-  
-  return ((a * h) / 2);
-  
 }
 
-console.log(triangle1Area);
+console.log(triangle3Area);
